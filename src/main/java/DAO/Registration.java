607bb32a12d8aed26b989bd6c7926e453979c85a
Roneet Michael <roneetmichael@yahoo.com>
2021-11-30 13:15:12 +0530

    Created MySQL Database connection class & created Registraion Servlet to insert Registration details into Database.

---
+++ src/main/java/DAO/Registration.java
@@ -0,0 +1,101 @@
+package DAO;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/registration")
+public class Registration extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        try{
+            System.out.println("Inside doPost");
+            Connection connection = DBConnection.getConnection();
+
+            // Getting Max partyId from Database
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("select MAX(partyId) as maxId from Party");
+
+            // Storing max value in variable Id
+            int maxId = -1;
+            if(result != null){
+                while (result.next()){
+                    maxId = result.getInt("maxId");
+                }
+            }
+            statement.close(); // Closing Statement
+
+            // Fetching values from Registration Page.
+            String firstName = req.getParameter("firstName");
+            String lastName = req.getParameter("lastName");
+            String city = req.getParameter("city");
+            int zip = Integer.parseInt(req.getParameter("zip"));
+            String state = req.getParameter("state");
+            String country = req.getParameter("country");
+            String phone = req.getParameter("phone");
+
+            String query = "INSERT INTO `Data_Modelling`.`Party` " +
+                    "(`partyId`, `firstName`, `lastName`, `city`, `zip`, `state`, `country`, `phone`) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, maxId+1);
+            preparedStatement.setString(2, firstName);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setString(4, city);
+            preparedStatement.setInt(5, zip);
+            preparedStatement.setString(6, state);
+            preparedStatement.setString(7, country);
+            preparedStatement.setString(8, phone);
+
+            String email =req.getParameter("email");
+            String password = req.getParameter("password");
+
+            String loginQuery = "INSERT INTO `Data_Modelling`.`UserLogin` " +
+                    "(`userLoginId`, `password`, `partyId`) " +
+                    "VALUES (?, ?, ?)";
+
+            PreparedStatement preparedStatement1 = connection.prepareStatement(loginQuery);
+            preparedStatement1.setString(1, email);
+            preparedStatement1.setString(2, password);
+            preparedStatement1.setInt(3, maxId+1);
+
+            String checkingEmail = "select partyId from UserLogin";
+            Statement statement1 = connection.createStatement();
+            ResultSet resultSet = statement1.executeQuery(checkingEmail);
+
+            while (resultSet.next()){
+                if(resultSet.getString("PartyId").equals(email)){
+                    throw new Exception("Email already exists");
+                }
+                else {
+                    // Setting values in Party Table
+                    preparedStatement.executeUpdate();
+                    System.out.println("Record inserted");
+                    preparedStatement.close();
+
+                    // Inserting values in UserLogin table
+                    preparedStatement1.executeUpdate();
+                    preparedStatement1.close(); // Closing Prepared Statement
+
+                }
+            }
+
+            connection.close(); // Closing Connection
+
+            PrintWriter out = resp.getWriter();
+            out.println("Inserted Successfully");
+
+        } catch (SQLException | ClassNotFoundException sqlException){
+            sqlException.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // Closing doPost Method
+}
