df9999f42d0ac022a40c773d51fb2db794eb1a43
Roneet Michael <roneetmichael@yahoo.com>
2021-12-02 12:07:03 +0530

    Implemented Authentication filter to prevent users from accessing profile page without logging in.

---
+++ src/main/java/DAO/AuthFilter.java
@@ -0,0 +1,24 @@
+package DAO;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+       HttpServletRequest req =  (HttpServletRequest) request;
+       HttpSession session = req.getSession();
+
+       String email = (String) session.getAttribute("email");
+       if(email != null) {
+           // If str (email) is not null. It means user is logged in
+           chain.doFilter(request, response);
+       } else {
+           RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+           request.setAttribute("errMsg", "Please login first");
+           requestDispatcher.include(request, response);
+       }
+    }
+}
